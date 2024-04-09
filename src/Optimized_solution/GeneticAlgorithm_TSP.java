24fc9bc6807b94f7619275bce0ead8ef69395744
Austen Lowder <austenswl@gmail.com>
2024-04-09 15:49:12 -0400

    Adds new files that were done in class

--- src/Optimized_solution/GeneticAlgorithm_TSP.java
+++ src/Optimized_solution/GeneticAlgorithm_TSP.java
@@ -1,131 +1,79 @@
 package Optimized_solution;
 
-import core_algorithms.Chromosome;
+import core_algorithms.GeneticAlgorithm;
+import core_algorithms.Individual;
+import optimization_problems.TSP;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
-public class GeneticAlgorithm_TSP {
-    private int populationSize;
-    private double mutationRate;
-    private int elitismCount;
-    private Random random;
+/**
+ * Implement elements that are problem specific
+ *
+ */
+public class GeneticAlgorithm_TSP
+        extends GeneticAlgorithm<Integer> {
+    private final TSP problem;
 
-    public GeneticAlgorithm_TSP(int populationSize, double mutationRate, int elitismCount) {
-        this.populationSize = populationSize;
-        this.mutationRate = mutationRate;
-        this.elitismCount = elitismCount;
-        this.random = new Random();
+    public GeneticAlgorithm_TSP(
+            int maxGen, double mRate, double elitism, TSP problem){
+        super(maxGen, mRate, elitism);
+        this.problem = problem;
     }
 
-    public void run(int chromosomeLength, int numGenerations) {
-        // Initialize population
-        List<Chromosome<Integer>> population = initPopulation(chromosomeLength);
-
-        // Evolve the population for a certain number of generations
-        for (int i = 0; i < numGenerations; i++) {
-            population = evolvePopulation(population);
-            System.out.println("Generation " + (i+1) +);
-            Chromosome<Object> getBestSolution = null;
-            System.out.println("Best Solution: " + getBestSolution.getGenes());
-        }
-
+    public double calcFitnessScore(
+            List<Integer> chromosome){
+        return 1/problem.cost(chromosome);
     }
 
-    private List<Chromosome<Integer>> initPopulation(int chromosomeLength) {
-        List<Chromosome<Integer>> population = new ArrayList<>();
-        for (int i = 0; i < populationSize; i++) {
-            Chromosome<Integer> newChromosome = new Chromosome<>(chromosomeLength);
-            newChromosome.generateChromosome();
-            population.add(newChromosome);
+    public Individual<Integer> reproduce(
+            Individual<Integer> p1, Individual<Integer> p2) {
+        //TODO
+
+        return p1;
+    }
+
+    public Individual<Integer> mutate(Individual<Integer> i){
+        //TODO
+        return i;
+    }
+
+    public List<Individual<Integer>> generateInitPopulation(
+            int popSize, int numCities ){
+        List<Individual<Integer>> population =
+                new ArrayList<>(popSize);
+        for(int i=0; i<popSize; i++){
+            List<Integer> chromosome = new ArrayList<>(numCities);
+            for(int j=0; j<numCities; j++){
+                chromosome.add(j);
+            }
+            Collections.shuffle(chromosome);
+            Individual<Integer> indiv = new Individual<>(
+                    chromosome, calcFitnessScore(chromosome));
+            population.add(indiv);
         }
         return population;
     }
 
-    private Chromosome<Integer> crossover(Chromosome<Integer> parent1, Chromosome<Integer> parent2) {
-        Chromosome<Integer> child = new Chromosome<>(parent1.getGenes().size());
-        int startPos = parent1.getGenes().isEmpty() ? 0 : random.nextInt(parent1.getGenes().size());
-        int endPos = parent1.getGenes().isEmpty() ? 0 : random.nextInt(parent1.getGenes().size());
+    public static void main(String[] args) {
+        int MAX_GEN = 200;
+        double MUTATION_RATE = 0.05;
+        int POPULATION_SIZE = 1000;
+        int NUM_CITES = 26; //choose from 5, 6, 17, 26
+        double ELITISM = 0.2;
 
-        for (int i = 0; i < child.getGenes().size(); i++) {
-            if (startPos < endPos && i > startPos && i < endPos) {
-                child.getGenes().set(i, parent1.getGenes().get(i));
-            } else if (startPos > endPos) {
-                if (!(i < startPos && i > endPos)) {
-                    child.getGenes().set(i, parent1.getGenes().get(i));
-                }
-            }
-        }
+        TSP problem = new TSP(NUM_CITES);
 
-        for (int i = 0; i < parent2.getGenes().size(); i++) {
-            if (!child.getGenes().contains(parent2.getGenes().get(i))) {
-                for (int ii = 0; ii < child.getGenes().size(); ii++) {
-                    if (child.getGenes().get(ii) == null) {
-                        child.getGenes().set(ii, parent2.getGenes().get(i));
-                        break;
-                    }
-                }
-            }
-        }
+        GeneticAlgorithm_TSP agent = new GeneticAlgorithm_TSP(
+                MAX_GEN, MUTATION_RATE, ELITISM, problem);
 
-        return child;
-    }
+        Individual<Integer> best =
+                agent.evolve(agent.generateInitPopulation(
+                        NUM_CITES, POPULATION_SIZE));
 
-    private void mutate(Chromosome<Integer> chromosome) {
-        for (int i = 0; i < chromosome.getGenes().size(); i++) {
-            if (random.nextDouble() <= mutationRate) {
-                int index1 = random.nextInt(chromosome.getGenes().size());
-                int index2 = random.nextInt(chromosome.getGenes().size());
-                Collections.swap(chromosome.getGenes(), index1, index2);
-            }
-        }
-    }
-
-    private List<Chromosome<Integer>> evolvePopulation(List<Chromosome<Integer>> population) {
-        List<Chromosome<Integer>> newPopulation = new ArrayList<>();
-
-        // Keep the elite individuals
-        for (int i = 0; i < elitismCount; i++) {
-            newPopulation.add(population.get(i));
-        }
-
-        // Crossover and mutate the rest of the population
-        for (int i = elitismCount; i < population.size(); i++) {
-            Chromosome<Integer> parent1 = population.get(random.nextInt(population.size()));
-            Chromosome<Integer> parent2 = population.get(random.nextInt(population.size()));
-            Chromosome<Integer> child = crossover(parent1, parent2);
-            mutate(child);
-            newPopulation.add(child);
-        }
-
-        return newPopulation;
-    }
-
-    private double calculateFitness(Chromosome<Integer> chromosome) {
-        List<Integer> genes = chromosome.getGenes();
-        double totalDistance = 0.0;
-        // Assuming genes represent the order of visiting cities in the TSP
-        for (int i = 0; i < genes.size() - 1; i++) {
-            totalDistance += Math.abs(genes.get(i) - genes.get(i+1));
-        }
-        totalDistance += Math.abs(genes.get(genes.size() - 1) - genes.get(0));
-        return 1.0 / totalDistance;
-    }
-
-    private Chromosome<Integer> getBestSolution(List<Chromosome<Integer>> population) {
-        Chromosome<Integer> bestSolution = population.get(0);
-        double bestFitness = calculateFitness(bestSolution);
-        for (Chromosome<Integer> chromosome : population) {
-            double fitness = calculateFitness(chromosome);
-            if (fitness > bestFitness) {
-                bestSolution = chromosome;
-                bestFitness = fitness;
-            }
-        }
-        return bestSolution;
+        System.out.println(best);
+        System.out.println(problem.cost(best.getChromosome()));
     }
 
 }
-
--- src/core_algorithms/GeneticAlgorithm.java
+++ src/core_algorithms/GeneticAlgorithm.java
@@ -1,87 +1,74 @@
 package core_algorithms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class GeneticAlgorithm<T> {
-    private int populationSize;
-    private double mutationRate;
-    private int elitismCount;
-    private Random random;
+/**
+ * implement elements that are independent of any specific problem
+ *
+ */
+public abstract class GeneticAlgorithm<G> {
+    private final int MAX_GEN;
+    private final double MUTATION_RATE;
+    private final double ELITISM;
 
-    public GeneticAlgorithm(int populationSize, double mutationRate, int elitismCount) {
-        this.populationSize = populationSize;
-        this.mutationRate = mutationRate;
-        this.elitismCount = elitismCount;
-        this.random = new Random();
+    public GeneticAlgorithm(int maxGen, double mRate, double elitism){
+        this.MAX_GEN = maxGen;
+        this.MUTATION_RATE = mRate;
+        this.ELITISM = elitism;
     }
 
-    public List<Chromosome<T>> initPopulation(int chromosomeLength) {
-        List<Chromosome<T>> population = new ArrayList<>();
-        for (int i = 0; i < populationSize; i++) {
-            Chromosome<T> newChromosome = new Chromosome<>(chromosomeLength);
-            newChromosome.generateChromosome();
-            population.add(newChromosome);
-        }
-        return population;
-    }
-
-    public Chromosome<T> crossover(Chromosome<T> parent1, Chromosome<T> parent2) {
-        Chromosome<T> child = new Chromosome<>(parent1.getGenes().size());
-        int startPos = random.nextInt(parent1.getGenes().size());
-        int endPos = random.nextInt(parent1.getGenes().size());
-
-        for (int i = 0; i < child.getGenes().size(); i++) {
-            if (startPos < endPos && i > startPos && i < endPos) {
-                child.getGenes().set(i, parent1.getGenes().get(i));
-            } else if (startPos > endPos) {
-                if (!(i < startPos && i > endPos)) {
-                    child.getGenes().set(i, parent1.getGenes().get(i));
+    public Individual<G> evolve (List<Individual<G>> initPopulation){
+        List<Individual<G>> population = initPopulation;
+        for(int generation = 1; generation <= MAX_GEN; generation++) {
+            List<Individual<G>> offspring = new ArrayList<>();
+            for (int i = 0; i < population.size(); i++) {
+                Individual<G> p1 = selectAParent(population);
+                Individual<G> p2 = selectAParent(population, p1);
+                Individual<G> child = reproduce(p1, p2);
+                if (new Random().nextDouble() <= MUTATION_RATE) {
+                    child = mutate(child);
                 }
+                offspring.add(child);
             }
-        }
-
-        for (int i = 0; i < parent2.getGenes().size(); i++) {
-            if (!child.getGenes().contains(parent2.getGenes().get(i))) {
-                for (int ii = 0; ii < child.getGenes().size(); ii++) {
-                    if (child.getGenes().get(ii) == null) {
-                        child.getGenes().set(ii, parent2.getGenes().get(i));
-                        break;
-                    }
-                }
+            Collections.sort(population);
+            Collections.sort(offspring);
+            List<Individual<G>> newPopulation = new ArrayList<>();
+            int e = (int) (ELITISM * population.size());
+            for (int i = 0; i < e; i++) {
+                newPopulation.add(population.get(i));
             }
-        }
+            for (int i = 0; i < population.size() - e; i++) {
+                newPopulation.add(offspring.get(i));
+            }
+            population = newPopulation;
+        }//end of outer for loop
 
-        return child;
+        Collections.sort(population);
+        return population.get(0);
     }
 
-    public void mutate(Chromosome<T> chromosome) {
-        for (int i = 0; i < chromosome.getGenes().size(); i++) {
-            if (random.nextDouble() <= mutationRate) {
-                T newGene = chromosome.generateGene();
-                chromosome.getGenes().set(i, newGene);
-            }
-        }
+    public abstract Individual<G> reproduce (
+            Individual<G> p1, Individual<G> p2);
+    public abstract Individual<G> mutate (Individual<G> i);
+    public abstract double calcFitnessScore(List<G> chromosome);
+
+    public Individual<G> selectAParent(
+            List<Individual<G>> population){
+        //TODO
+        return null;
     }
 
-    public List<Object> evolvePopulation(List<Chromosome<T>> population) {
-        List<Object> newPopulation = new ArrayList<>();
-
-        // Keep the elite individuals
-        for (int i = 0; i < elitismCount; i++) {
-            newPopulation.add(population.get(i));
-        }
-
-        // Crossover and mutate the rest of the population
-        for (int i = elitismCount; i < population.size(); i++) {
-            Chromosome<T> parent1 = population.get(random.nextInt(population.size()));
-            Chromosome<T> parent2 = population.get(random.nextInt(population.size()));
-            Chromosome<T> child = crossover(parent1, parent2);
-            mutate(child);
-            newPopulation.add(child);
-        }
-
-        return newPopulation;
+    //optional, select a parent that's not p.
+    public Individual<G> selectAParent(
+            List<Individual<G>> population, Individual<G> p){
+     //TODO
+        return p;
     }
+
+
+
+
 }
